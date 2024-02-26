03d5a0c7bbea9cfcd102cb65c00a5e0dd7f68994
thanyapoj.tri <thanyapoj.tri@mahidol.edu>
2024-02-26 23:26:50 +0700

    [change] machine to dropdown

--- Create-Table-in-React/src/Table.jsx
+++ Create-Table-in-React/src/Table.jsx
@@ -4,19 +4,24 @@ import './App.css'
 
 function Table() {
     const [data, setData] = useState([])
+    const [machines, setMachines] = useState([]);
     const [name, setName] = useState('')
-    const [machine, setMachine] = useState('')
+    const [machine, setMachine] = useState('') // Updated to hold a single value
     const [editID, setEditID] = useState(-1)
 
     useEffect(()=>{
         axios.get('http://localhost:3000/users')
-        .then(res => setData(res.data))
+        .then(res => setData(res.data || []))
+        .catch(er => console.log(er));
+    }, [])
+    useEffect(()=>{
+        axios.get('http://localhost:3000/machines')
+        .then(res => setMachines(res.data || []))
         .catch(er => console.log(er));
     }, [])
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // id = data.length + 1;
         axios.post('http://localhost:3000/users', {name: name, machine: machine})
         .then(res => {
             location.reload()
@@ -27,52 +32,54 @@ function Table() {
         
     }
 
-    const handleDelte = (id) => {
-        console.log(id);
+    const handleDelete = (id) => {
         axios.delete('http://localhost:3000/users/'+id)
         .then(res => {
             location.reload();
-            console.log('deleted lew');
+            console.log('deleted');
         })
         .catch(er => console.log(er));
     }
 
-  return (
-    <div className='container'>
-        <div className='form-div'>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder='Enter Name' onChange={e => setName(e.target.value)}/>
-                <input type="text" placeholder='Enter Machine' onChange={e => setMachine(e.target.value)}/>
-                <button>Add</button> 
-            </form>
-        </div>
-      <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Machine</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                data.map((user, index) => (
-                    <tr key={index}>
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.machine}</td>
-                        <td>
-                            {/* <button onClick={() => handleEdit(user.id)}>edit</button> */}
-                            <button onClick={() => handleDelte(user.id)}>return</button>
-                        </td>
+    return (
+        <div className='container'>
+            <div className='form-div'>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Enter Name' onChange={e => setName(e.target.value)}/>
+                    <select value={machine} onChange={(e) => setMachine(e.target.value)}>
+                        {machines.map(machine => (
+                            <option key={machine.id} value={machine.value}>
+                                {machine.value}
+                            </option>
+                        ))}
+                    </select>
+                    <button>Add</button> 
+                </form>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Machine</th>
+                        <th>Action</th>
                     </tr>
-                ))
-            }
-        </tbody>
-      </table>
-    </div>
-  )
+                </thead>
+                <tbody>
+                    {data.map((user, index) => (
+                        <tr key={index}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.machine}</td>
+                            <td>
+                                <button onClick={() => handleDelete(user.id)}>return</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Table
